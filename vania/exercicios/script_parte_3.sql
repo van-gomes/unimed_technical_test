c7c5ed2605efcb5322788f44521bc72f612bb4f7
van-gomes <nninha.gomes@gmail.com>
2024-08-16 16:30:13 -0300

    Format database script

--- vania/exercicios/script_parte_3.sql
+++ vania/exercicios/script_parte_3.sql
@@ -2,12 +2,12 @@ create database avaliacao_programador;
 
 drop database avaliacao_programador;
 
-CREATE TABLE CLIENTE (
-ID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-NOME VARCHAR(80) NOT NULL, 
-EMAIL VARCHAR(80) NOT NULL, 
-POSSUI_PLANO CHAR(1), 
-PLANO VARCHAR(80)
+create table cliente (
+id integer auto_increment not null primary key, 
+nome varchar(80) not null, 
+email varchar(80) not null, 
+possui_plano char(1), 
+plano varchar(80)
 );
 
 select * from cliente;
@@ -16,37 +16,39 @@ select * from cliente;
 2.a) Inserir dois novos registros na tabela "Produtos" comos seguintes dados:
  ID do produto = 101, Nome = "Laptop", Preço = 1200
  ID do produto = 102, Nome = "Mouse", Preço = 50
- 
+ */
+
+create table produtos (
+id integer not null primary key, 
+nome varchar(50) not null,
+preco decimal(5,2) not null
+);
+
+insert into produtos(id, nome, preco)
+values (101, 'laptop', 1.200);
+
+insert into produtos(id, nome, preco)
+values (102, 'mouse', 50);
+
+/* 
 2.b) Agora, suponha que o preço do produto "Laptop" tenha aumentadopara 1500.
  Escreva um comando SQL para atualizar esse valor na tabela"Produtos
 */
 
-create table Produtos (
-ID INTEGER NOT NULL PRIMARY KEY, 
-NOME varchar(50) not null,
-PRECO decimal(5,2) not null
-);
-
-insert INTO Produtos(id, nome, preco)
-values (101, 'Laptop', 1.200);
-
-insert INTO Produtos(id, nome, preco)
-values (102, 'Mouse', 50);
-
-update Produtos
+update produtos
 set preco = 1.500
 where id = 101;
 
-select * from Produtos;
+select * from produtos;
 
-/* Exercício 3 */
-create table Pedidos (
-id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+/* exercício 3 */
+create table pedidos (
+id integer auto_increment not null primary key, 
 produto varchar(50),
 quantidade int(10),
-foreign key (ID) references Cliente (ID)
+foreign key (id) references cliente (id)
 );
 
-select nome C, produto P, quantidade P
-from Cliente C, Pedidos P
-where C.id = P.id;
+select nome c, produto p, quantidade p
+from cliente c, pedidos p
+where c.id = p.id;
