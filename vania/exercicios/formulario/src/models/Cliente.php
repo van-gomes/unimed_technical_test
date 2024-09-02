10b5440b7019a50889d897fc7159f4935732e161
van-gomes <nninha.gomes@gmail.com>
2024-09-02 20:55:35 -0300

    Construct Client Class

---
+++ vania/exercicios/formulario/src/models/Cliente.php
@@ -0,0 +1,29 @@
+class Cliente {
+    private $nome;
+    private $email;
+    private $possuiPlano;
+    private $nomePlano;
+
+    public function __construct($nome, $email, $possuiPlano, $nomePlano = null) {
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->possuiPlano = $possuiPlano;
+        $this->nomePlano = $nomePlano;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPossuiPlano() {
+        return $this->possuiPlano;
+    }
+
+    public function getNomePlano() {
+        return $this->nomePlano;
+    }
+}
